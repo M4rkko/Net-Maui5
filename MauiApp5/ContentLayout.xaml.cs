b10961e39e0efafd35ee847d008afe0d7fc76482
M4rkko <Markkogaming@gmail.com>
2024-10-28 00:24:03 +0300

    Navigeerimis menu = yes

--- MauiApp5/ContentLayout.xaml.cs
+++ MauiApp5/ContentLayout.xaml.cs
@@ -1,13 +1,16 @@
+using System.Windows.Input;
+
 namespace MauiApp5;
 
 public partial class ContentLayout : ContentView
 {
+ 
 	public ContentLayout()
 	{
+       
 		InitializeComponent();
-	}
-    private async void GoToMain(object sender, EventArgs args)
-    {
-        await Navigation.PushAsync(new NewPage3());
+        BindingContext = new MenuEvents(Navigation);
     }
+        
+    
 }
---
+++ MauiApp5/MenuEvents.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Input;
+
+namespace MauiApp5
+{
+    class MenuEvents
+    {
+        public ICommand GoToCommand { get; }
+        public INavigation Navigation { get; set; }
+        public MenuEvents(INavigation navigation)
+        {
+            GoToCommand = new Command<string>(GoToPage);
+            Navigation = navigation;
+        }
+        private async void GoToPage(string pageName)
+        {
+            ////if (pageName.CompareTo("Page-3") == 0)
+            //{
+                //await Navigation.PushAsync(new NewPage3());
+            //}
+            switch (pageName)
+            {
+                case "Page-1":
+                    await Navigation.PushAsync(new NewPage1());
+                    break;
+
+                case "Page-2":
+                    await Navigation.PushAsync(new NewPage2());
+                    break;
+
+                case "Page-3":
+                    await Navigation.PushAsync(new NewPage3());
+                    break;
+
+                case "Page-4":
+                    await Navigation.PushAsync(new NewPage4());
+                    break;
+
+                case "Page-5":
+                    await Navigation.PushAsync(new NewPage5());
+                    break;
+            }
+        }
+    }
+}
